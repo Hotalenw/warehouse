37f9d549fba9cab104456890b25f374b4d850a33
whotalen <whotalen@cisco.com>
2021-11-18 19:53:30 -0500

    Wip working start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import {Table} from 'reactstrap';
+import {Row} from "./components/Row";
+import WarehouseService from "./services/WarehouseService";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  state = {
+    warehouses: []
+  }
+  componentWillMount() {
+    // Lets get our warehouses from the api.
+    WarehouseService.getWarehouseServiceInstance().getWarehouses().then((res) => {
+        const warehouses = res.data;
+        if (warehouses) {
+            this.setState({warehouses} )
+        }
+    });
+  }
+  render() {
+      return (
+        <div className="container">
+          <Table className="table table-responsive table-bordered">
+            <thead>
+              <tr className="text-center">
+                  <th>Warehouse Id</th>
+                  <th>Warehouse Name</th>
+                  <th>Warehouse Description</th>
+                  <th>Warehouse Address</th>
+                  <th>Actions</th>
+              </tr>
+            </thead>
+
+            <tbody>
+                { this.state.warehouses.map((warehouse) => <Row warehouse={warehouse}/>) }
+            </tbody>
+          </Table>
+        </div>
+      );
+   }
 }
 
 export default App;
