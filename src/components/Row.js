56ae78f5a4b4436dcccc59fe2b27576fc470c1c6
Wesley Hotalen <whotalen@cisco.com>
2021-11-22 10:41:28 -0500

    Auto stash before checking out "origin/main"

---
+++ src/components/Row.js
@@ -0,0 +1,58 @@
+import React, {Component} from "react";
+import {Button} from "reactstrap";
+import {EventEmitter} from "../events";
+import {constants} from "../constants/constants";
+
+/**
+ * Represents a single row of our warehouse data.
+ */
+export class Row extends Component {
+    warehouse = this.props.warehouse;
+    render() {
+        return (
+            <tr style={{ verticalAlign: 'middle' }}>
+                <td className="text-center">{ this.warehouse.warehouseId }</td>
+                <td className="text-center">{ this.warehouse.warehouseName }</td>
+                <td className="text-center">{ this.warehouse.warehouseDescription }</td>
+                <td className="text-center">
+                    {this.warehouse.warehouseAddress?.buildingName} <br/>
+                    <div style={{ padding: '.5rem' }}>
+                        {this.warehouse.warehouseAddress?.streetLine1 ?
+                        <span>{this.warehouse.warehouseAddress?.streetLine1}, </span> : null}
+                        <span>{this.warehouse.warehouseAddress?.streetLine2}</span>
+                        {this.warehouse.warehouseAddress?.city ?
+                            <span><br/>{this.warehouse.warehouseAddress?.city}, </span> : null}
+                        {this.warehouse.warehouseAddress?.stateProvince ?
+                            <span>{this.warehouse.warehouseAddress?.stateProvince} </span> : null}
+                        {this.warehouse.warehouseAddress?.zipPostalCode ?
+                            <span>{this.warehouse.warehouseAddress?.zipPostalCode} </span> : null}
+                        {this.warehouse.warehouseAddress?.country ?
+                            <span><br/>{this.warehouse.warehouseAddress?.country} </span> : null}
+                    </div>
+                </td>
+                <td className="text-center button-grid">
+                    <Button color="success" size="sm" className="mr-2" onClick={() => this.handleEdit()}>Edit</Button>
+                    <Button style={{ marginLeft: '.5rem' }} color="danger" size="sm" onClick={() => this.handleDelete()}>Delete</Button>
+                </td>
+            </tr>
+
+        );
+    }
+
+    /**
+     * Handles a delete event.
+     */
+    handleDelete() {
+        // eslint-disable-next-line no-restricted-globals
+        if (confirm(`Are you sure you want to delete warehouse ${this.warehouse.warehouseName}`)) {
+            EventEmitter.dispatch(constants.REMOVE_WAREHOUSE, this.warehouse.warehouseId);
+        }
+    }
+
+    /**
+     * Emits an event that an edit operation needs to occur.
+     */
+    handleEdit() {
+        EventEmitter.dispatch(constants.EDIT_WAREHOUSE, this.warehouse);
+    }
+}
---
+++ src/components/WarehouseForm.js
@@ -0,0 +1,116 @@
+import React, {Component} from 'react';
+import {Form, FormGroup, Input, Label, Button} from "reactstrap";
+import {EventEmitter} from "../events";
+import {constants} from "../constants/constants";
+import './WarehouseForm.css'
+
+
+/**
+ * Represents a warehouse form.
+ */
+export class WarehouseForm extends Component {
+    state = {
+        warehouse: this.props.warehouse,
+        name: this.props.warehouse.warehouseName ?? '',
+        description: this.props.warehouse?.warehouseDescription ?? '',
+        buildingName: this.props.warehouse?.warehouseAddress?.buildingName ?? '',
+        streetLine1: this.props.warehouse?.warehouseAddress?.streetLine1 ?? '',
+        streetLine2: this.props.warehouse?.warehouseAddress?.streetLine2 ?? '',
+        city: this.props.warehouse.warehouseAddress?.city ?? '',
+        stateProvince: this.props.warehouse.warehouseAddress?.stateProvince ?? '',
+        zipPostalCode: this.props.warehouse.warehouseAddress?.zipPostalCode ?? '',
+        country: this.props.warehouse.warehouseAddress?.country ?? ''
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="text-center">
+                    <span className="h5">Edit '{this.state.warehouse?.warehouseName}'</span>
+                </div>
+                <div className="container">
+                    <Form>
+                        <FormGroup className="field-grid">
+                            <div>
+                                <Label name="name"> Warehouse Name</Label>
+                                <Input name="name" type="text" value={this.state.name} onChange={this.handleChange.bind(this)}/>
+                            </div>
+                            <div>
+                                <Label name="description"> Warehouse Description</Label>
+                                <Input name="description" type="text" value={this.state.description} onChange={this.handleChange.bind(this)}/>
+                            </div>
+                        </FormGroup>
+                        <Label> Warehouse Address</Label>
+                        <div className="container border">
+                            <FormGroup className="address-grid">
+                                <div>
+                                    <Label name="buildingName"> Building Name</Label>
+                                    <Input name="buildingName" type="text" value={this.state.buildingName} onChange={this.handleChange.bind(this)}/>
+                                </div>
+                                <div>
+                                    <Label name="streetLine1"> StreetLine 1</Label>
+                                    <Input name="streetLine1" type="text" value={this.state.streetLine1} onChange={this.handleChange.bind(this)}/>
+                                </div>
+                                <div>
+                                    <Label name="streetLine2"> StreetLine 2</Label>
+                                    <Input name="streetLine2" type="text" value={this.state.streetLine2} onChange={this.handleChange.bind(this)}/>
+                                </div>
+                                <div>
+                                    <Label name="city"> City</Label>
+                                    <Input name="city" type="text" value={this.state.city} onChange={this.handleChange.bind(this)}/>
+                                </div>
+                                <div>
+                                    <Label name="stateProvince"> State / Province</Label>
+                                    <Input name="stateProvince" type="text" value={this.state.stateProvince} onChange={this.handleChange.bind(this)}/>
+                                </div>
+                                <div>
+                                    <Label name="zipPostalCode"> Zip Code</Label>
+                                    <Input name="zipPostalCode" type="text" value={this.state.zipPostalCode} onChange={this.handleChange.bind(this)}/>
+                                </div>
+                                <div>
+                                    <Label name="country"> Country</Label>
+                                    <Input name="country" type="text" value={this.state.country} onChange={this.handleChange.bind(this)}/>
+                                </div>
+                            </FormGroup>
+                        </div>
+                    </Form>
+                    <div className="text-center" style={{margin: '1rem'}}>
+                        <Button className="btn" onClick={this.onSubmit.bind(this)} type="submit">Submit</Button>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+    /**
+     * Handling change events from the form.
+     * @param event
+     */
+    handleChange(event) {
+        this.setState({
+            [event.target?.name]: event.target?.value
+        });
+    }
+
+    /**
+     * Handles submitting the form.
+     */
+    onSubmit() {
+        this.setState({
+            warehouse: Object.assign(this.state.warehouse, {
+                warehouseName: this.state.name,
+                warehouseDescription: this.state.description,
+                warehouseAddress: {
+                    buildingName: this.state.buildingName,
+                    streetLine1: this.state.streetLine1,
+                    streetLine2: this.state.streetLine2,
+                    city: this.state.city,
+                    stateProvince: this.state.stateProvince,
+                    zipPostalCode: this.state.zipPostalCode,
+                    country: this.state.country
+                }
+            })
+        })
+        EventEmitter.dispatch(constants.UPDATE_WAREHOUSE,this.state.warehouse);
+    }
+}
